f6a2275d554b03edb3137995190af4c4e22d1bb3
YoshiClaroo <braianm313@gmail.com>
2025-05-23 18:02:26 -0300

    Create firebase.js

---
+++ public/js/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/11.8.1/firebase-firestore.js';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCgNxF0TYCUc_t3raN37ypEC6KHBqJIXuU",
+    authDomain: "video-pop-13c53.firebaseapp.com",
+    projectId: "video-pop-13c53",
+    storageBucket: "video-pop-13c53.firebasestorage.app",
+    messagingSenderId: "682309905603",
+    appId: "1:682309905603:web:2b84ac53d103cba0558676",
+    measurementId: "G-42VFTCV7ME"
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
